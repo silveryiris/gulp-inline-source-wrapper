132a4a49ed7a520f289bc601895122242ad086a4
silveryiris <silveryiris@gmail.com>
2018-05-15 11:21:01 +0800

    Update dev deps

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ var _package = require("./package.json");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function inlineSourceWrapper(options = {}) {
   const stream = _through.default.obj(async (file, enc, cb) => {
     if (file.isNull() || file.isDirectory()) {
@@ -34,9 +38,8 @@ function inlineSourceWrapper(options = {}) {
       "rootpath": _path.default.dirname(file.path),
       "htmlpath": file.path
     };
-    const pluginOptions = { ...defaultOptions,
-      ...options
-    };
+
+    const pluginOptions = _objectSpread({}, defaultOptions, options);
 
     try {
       const html = await (0, _inlineSource.inlineSource)(file.contents.toString(), pluginOptions);
